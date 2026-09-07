353a8c832974fd5793975b1b0228d94ef3c6d2ca
Apple <Apple@MacBookPro.lan>
2026-09-07 09:48:45 +0530

    Fix Image.file layout constraints in Site Not Ready dialog

--- Service Management/Technician Mobile App/lib/main.dart
+++ Service Management/Technician Mobile App/lib/main.dart
@@ -1551,7 +1551,7 @@ class _JobDetailsScreenState extends State<JobDetailsScreen> {
                         child: sitePhoto != null
                             ? ClipRRect(
                                 borderRadius: BorderRadius.circular(12),
-                                child: Image.file(sitePhoto!, fit: BoxFit.cover, width: double.infinity),
+                                child: Image.file(sitePhoto!, fit: BoxFit.cover, width: double.infinity, height: double.infinity),
                               )
                             : Column(
                                 mainAxisAlignment: MainAxisAlignment.center,
